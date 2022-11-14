8b49d8e02b187acd996e61022cab8abb93066885
joe <20902932+osekojoe@users.noreply.github.com>
2022-11-14 13:58:06 -0800

    main file

--- main.c
+++ main.c
@@ -17,11 +17,59 @@ void sig_handler(int sig)
 }
 
 /**
-*
-*
-*
+*execute - excecute a command in a child process 
+* @args: arguments array
+* @front: double pointer to the beginning of args
+* Return: erro? corresponding error code; otherwise,
+* exit value of the last command
 */
 int execute(char **args, char **front)
 {
-	
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Child error : ");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free (command);
+	return (ret);
 }
