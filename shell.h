459d7bcbdde75a6c2e7fbb1cd30c97a39be9697f
joe <20902932+osekojoe@users.noreply.github.com>
2022-11-16 02:08:39 -0800

    function prototypes

--- shell.h
+++ shell.h
@@ -1,9 +1,12 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#define ENF_OF_FILE -2
-#define EXIT -3
+/*
+ * File: shell
+ */
 
+#include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -11,10 +14,11 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
-#include <signal.h>
-#include <fcntl.h>
 
-/* global environment */
+#define END_OF_FILE -2
+#define EXIT -3
+
+/* Global environemnt */
 extern char **environ;
 /* Global program name */
 char *name;
@@ -22,21 +26,21 @@ char *name;
 int hist;
 
 /**
-* struct list_s - struct defining a linked list
-* @dir: directory path
-* @next: point to the next struct list_s
-*/
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
+ */
 typedef struct list_s
 {
 	char *dir;
-	struct lists *next;
+	struct list_s *next;
 } list_t;
 
 /**
-* struct builtin_s - struct type for builtin commands
-* @name: name of builtin command
-* @f: function pointer to the builtin command's function
-*/
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
+ */
 typedef struct builtin_s
 {
 	char *name;
@@ -44,11 +48,11 @@ typedef struct builtin_s
 } builtin_t;
 
 /**
-* alias_s - struct type defining aliases
-* @name: alias name
-* @value: alias value
-* @next: pointer to the next alias
-*/
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
+ */
 typedef struct alias_s
 {
 	char *name;
@@ -56,10 +60,10 @@ typedef struct alias_s
 	struct alias_s *next;
 } alias_t;
 
-/* global alias linked list*/
+/* Global aliases linked list */
 alias_t *aliases;
 
-/*main helpers */
+/* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_strtok(char *line, char *delim);
@@ -69,7 +73,7 @@ int execute(char **args, char **front);
 void free_list(list_t *head);
 char *_itoa(int num);
 
-/*input helpers*/
+/* Input Helpers */
 void handle_line(char **line, ssize_t read);
 void variable_replacement(char **args, int *exe_ret);
 char *get_args(char *line, int *exe_ret);
@@ -80,7 +84,7 @@ int check_args(char **args);
 void free_args(char **args, char **front);
 char **replace_aliases(char **args);
 
-/*string funcs */
+/* String functions */
 int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
@@ -90,7 +94,7 @@ int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
-/*Builtin functions*/
+/* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
 int shellby_env(char **args, char __attribute__((__unused__)) **front);
@@ -100,12 +104,12 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front);
 int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-/*Builtin helpers */
+/* Builtin Helpers */
 char **_copyenv(void);
 void free_env(void);
 char **_getenv(const char *var);
 
-/* Error handling */
+/* Error Handling */
 int create_error(char **args, int err);
 char *error_env(char **args);
 char *error_1(char **args);
@@ -121,7 +125,6 @@ void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
 
-/*help */
 void help_all(void);
 void help_alias(void);
 void help_cd(void);
@@ -133,5 +136,4 @@ void help_unsetenv(void);
 void help_history(void);
 
 int proc_file_commands(char *file_path, int *exe_ret);
-
 #endif
