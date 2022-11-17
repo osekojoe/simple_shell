c1086107939345ec23bab769160cb52333a57477
joe <20902932+osekojoe@users.noreply.github.com>
2022-11-17 11:59:08 -0800

    string functions

--- str_funcs1.c
+++ str_funcs1.c
@@ -10,26 +10,27 @@ char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
 
 /**
- * _strlen - get string length
- * @s: string pointer
- * Return: string length
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the characters string.
+ *
+ * Return: The length of the character string.
  */
 int _strlen(const char *s)
 {
-	int len = 0;
+	int length = 0;
 
 	if (!s)
-		return (len);
-	for (len = 0; s[len]; len++)
+		return (length);
+	for (length = 0; s[length]; length++)
 		;
-	return (len);
+	return (length);
 }
 
 /**
  * _strcpy - Copies the string pointed to by src, including the
  *           terminating null byte, to the buffer pointed by des.
  * @dest: Pointer to the destination of copied string.
- * @src: Pointer to string to be copied.
+ * @src: Pointer to the src of the source string.
  *
  * Return: Pointer to dest.
  */
@@ -40,7 +41,6 @@ char *_strcpy(char *dest, const char *src)
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
-
 	return (dest);
 }
 
@@ -57,7 +57,7 @@ char *_strcat(char *dest, const char *src)
 	const char *srcTemp;
 
 	destTemp = dest;
-	srcTemp = src;
+	srcTemp =  src;
 
 	while (*destTemp != '\0')
 		destTemp++;
@@ -65,9 +65,9 @@ char *_strcat(char *dest, const char *src)
 	while (*srcTemp != '\0')
 		*destTemp++ = *srcTemp++;
 	*destTemp = '\0';
-
 	return (dest);
 }
+
 /**
  * _strncat - Concantenates two strings where n number
  *            of bytes are copied from source.
@@ -83,7 +83,7 @@ char *_strncat(char *dest, const char *src, size_t n)
 	size_t i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + 1] = src[i];
+		dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
 
 	return (dest);
